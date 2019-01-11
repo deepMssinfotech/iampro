dd303531122fe3e92d5c80bb77eb61fbbcd89503
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-11 19:29:24 +0530

    commit

--- app/src/main/java/co/iampro/mssinfotech/com/iampro/LoadingActivity.java
+++ app/src/main/java/co/iampro/mssinfotech/com/iampro/LoadingActivity.java
@@ -1,7 +1,11 @@
 package co.iampro.mssinfotech.com.iampro;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(LoadingActivity.this, WelcomeActivity.class);
+                LoadingActivity.this.startActivity(mainIntent);
+                LoadingActivity.this.finish();
+            }
+        }, 3000);
     }
 }
+
+
