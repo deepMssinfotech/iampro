09be9812ec5de8b7a1c3ca3f7a4ad4613eb996a4
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-12 13:24:51 +0530

    commit

---
+++ app/src/main/java/com/mssinfotech/iampro/co/user/AddProvideActivity.java
@@ -0,0 +1,14 @@
+package com.mssinfotech.iampro.co.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mssinfotech.iampro.co.R;
+
+public class AddProvideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_provide);
+    }
+}
