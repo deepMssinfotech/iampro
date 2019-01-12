8de5276f9b7426981409d0080f6791003f7d2f13
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-12 20:53:39 +0530

    deep commit

--- app/src/main/java/com/mssinfotech/iampro/co/user/MyProvideActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/user/MyProvideActivity.java
@@ -1,7 +1,9 @@
 package com.mssinfotech.iampro.co.user;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,5 +40,10 @@ public class MyProvideActivity extends AppCompatActivity {
                 .error(R.drawable.profile_background)
                 .into(userbackgroud);
 
+    }
+
+    public void redirect(View v){
+        Intent i_signup = new Intent(MyProvideActivity.this,AddProvideActivity.class);
+        MyProvideActivity.this.startActivity(i_signup);
     }
 }
--- app/src/main/java/com/mssinfotech/iampro/co/user/MyVideoActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/user/MyVideoActivity.java
@@ -1,7 +1,9 @@
 package com.mssinfotech.iampro.co.user;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,5 +40,9 @@ public class MyVideoActivity extends AppCompatActivity {
                 .error(R.drawable.profile_background)
                 .into(userbackgroud);
 
+    }
+    public void redirect(View v){
+        Intent i_signup = new Intent(MyVideoActivity.this,AddVideoActivity.class);
+        MyVideoActivity.this.startActivity(i_signup);
     }
 }
