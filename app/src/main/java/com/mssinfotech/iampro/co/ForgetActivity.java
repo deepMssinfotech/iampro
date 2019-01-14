37e57b7595744660b5b688fce0cc7982d77b4dc2
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-14 15:26:15 +0530

    deep commit

--- app/src/main/java/com/mssinfotech/iampro/co/ForgetActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/ForgetActivity.java
@@ -45,9 +45,9 @@ public class ForgetActivity extends AppCompatActivity   implements View.OnClickL
         setContentView(R.layout.activity_forget);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+        tilemail= findViewById(R.id.tilemail);
 
-        btnforgetProcess = findViewById(R.id.btnforgetProcess);
-        btnforgetProcess.setOnClickListener(this);
+        etemail = findViewById(R.id.etemail);
 
     }
 
@@ -58,90 +58,84 @@ public class ForgetActivity extends AppCompatActivity   implements View.OnClickL
                 Intent i_login = new Intent(ForgetActivity.this, LoginActivity.class);
                 ForgetActivity.this.startActivity(i_login);
                 break;
-            case R.id.btnforgetProcess:
-                // get click action
-                Log.d("btnforgetProcess","btnforgetProcess");
-                if (validateemail()) {
-                    Log.d("btnforgetProcess","btnforgetProcess");
-                    sendOtp(v);
-                }
-                break;
+
             default:
                 break;
         }
     }
     public void sendOtp(View v){
-        if (!Config.haveNetworkConnection(this)){
-            Config.showInternetDialog(this);
-            return;
-        }
-        randomNumber = r.nextInt(10000);
-        final String url=Config.API_URL+"ajax.php";
-        //tv.setText(String.valueOf(randomNumber));
-        final ProgressDialog loading = ProgressDialog.show(this,"Processing...","Please wait...",false,false);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String s) {
-                        JSONObject result = null;
-                        loading.dismiss();
-                        try {
-
-                            //Parsing the fetched Json String to JSON Object
-                            result = new JSONObject(s);
-                            String status=result.getString("status");
-                            String msg=result.getString("msg");
-                            if(status.equals("success")) {
-                                String vcode = result.getString("vcode");
-                                Log.d("Lresponse", "" + vcode);
-                                Intent intent = new Intent(getApplicationContext(), OtpForgetActivity.class);
-                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                intent.putExtra("vcode", vcode);
-                                intent.putExtra("email", etemail.getText().toString().trim());
-                                startActivity(intent);
-                                finish();
-                            }else{
-                                Toast.makeText(ForgetActivity.this, msg , Toast.LENGTH_LONG).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                        //Showing toast message of the response
-                        //Toast.makeText(LoginActivity.this, s , Toast.LENGTH_LONG).show();
-
-
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        //Dismissing the progress dialog
-                        loading.dismiss();
-                        Log.d("error",volleyError.getMessage()+"--" + url.toString());
-                        Toast.makeText(getApplicationContext(),volleyError.getMessage(),Toast.LENGTH_LONG).show();
-                    }
-                }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                //Converting Bitmap to String
-                //String image = getStringImage(fingerData.FingerImage());
-                //Creating parameters
-                Map<String,String> params = new Hashtable<String, String>();
-
-                params.put("type","sendotp");
-                params.put("otp",String.valueOf(randomNumber));
-                //params.put("mobile",etMobile.getText().toString());
-                //returning parameters
-                return params;
+        if (validateemail()) {
+            if (!Config.haveNetworkConnection(this)) {
+                Config.showInternetDialog(this);
+                return;
             }
-        };
-        //Creating a Request Queue
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+            randomNumber = r.nextInt(10000);
+            final String url = Config.API_URL + "ajax.php";
+            //tv.setText(String.valueOf(randomNumber));
+            final ProgressDialog loading = ProgressDialog.show(this, "Processing...", "Please wait...", false, false);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            JSONObject result = null;
+                            loading.dismiss();
+                            try {
 
-        //Adding request to the queue
-        requestQueue.add(stringRequest);
+                                //Parsing the fetched Json String to JSON Object
+                                result = new JSONObject(s);
+                                String status = result.getString("status");
+                                String msg = result.getString("msg");
+                                if (status.equals("success")) {
+                                    String vcode = result.getString("vcode");
+                                    Log.d("Lresponse", "" + vcode);
+                                    Intent intent = new Intent(getApplicationContext(), OtpForgetActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    intent.putExtra("vcode", vcode);
+                                    intent.putExtra("email", etemail.getText().toString().trim());
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(ForgetActivity.this, msg, Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            //Showing toast message of the response
+                            //Toast.makeText(LoginActivity.this, s , Toast.LENGTH_LONG).show();
+
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            //Dismissing the progress dialog
+                            loading.dismiss();
+                            Log.d("error", volleyError.getMessage() + "--" + url.toString());
+                            Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    //Converting Bitmap to String
+                    //String image = getStringImage(fingerData.FingerImage());
+                    //Creating parameters
+                    Map<String, String> params = new Hashtable<String, String>();
+
+                    params.put("type", "sendotp");
+                    params.put("otp", String.valueOf(randomNumber));
+                    //params.put("mobile",etMobile.getText().toString());
+                    //returning parameters
+                    return params;
+                }
+            };
+            //Creating a Request Queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            //Adding request to the queue
+            requestQueue.add(stringRequest);
+        }
     }
     private boolean validateemail() {
         final String data = etemail.getText().toString();
