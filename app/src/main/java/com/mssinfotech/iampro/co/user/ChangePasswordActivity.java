fb4e888bb3d49b28c1d4eac7bd1a000ab02cad32
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-02-05 10:37:39 +0530

    Merge branch 'master' of https://github.com/deepMssinfotech/iampro
    # Conflicts:
    #	app/src/main/java/com/mssinfotech/iampro/co/common/Config.java
    #	app/src/main/java/com/mssinfotech/iampro/co/common/IncludeShortMenu.java
    #	app/src/main/java/com/mssinfotech/iampro/co/common/UserMenuActivity.java
    #	app/src/main/java/com/mssinfotech/iampro/co/user/JoinedFriendsActivity.java

--- app/src/main/java/com/mssinfotech/iampro/co/user/ChangePasswordActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/user/ChangePasswordActivity.java
@@ -1,17 +1,167 @@
 package com.mssinfotech.iampro.co.user;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.mssinfotech.iampro.co.LoginActivity;
+import com.mssinfotech.iampro.co.OtpForgetActivity;
 import com.mssinfotech.iampro.co.R;
 import com.mssinfotech.iampro.co.common.Config;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.acl.Group;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 public class ChangePasswordActivity extends AppCompatActivity {
 
+
+    List<String> list = new ArrayList<String>();
+
+    private Button btnSave;
+    private EditText etold_pass;
+    private EditText etnew_pass;
+    private EditText etconform_pass;
+   // private TextInputLayout tilotp,tilnpassword,tilcpassword;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
-        Config.setLayoutName(getResources().getResourceEntryName(R.layout.activity_change_password));
+
+        etold_pass = findViewById(R.id.etold_pass);
+        etnew_pass = findViewById(R.id.etnew_pass);
+        etconform_pass = findViewById(R.id.etconform_pass);
+
     }
+    public void update_password(View v){
+        if (validatenpassword()) {
+            if (!Config.haveNetworkConnection(this)) {
+                Config.showInternetDialog(this);
+                return;
+            }
+            randomNumber = r.nextInt(10000);
+            final String url = Config.API_URL + "app_service.php";
+            //tv.setText(String.valueOf(randomNumber));
+            final ProgressDialog loading = ProgressDialog.show(this, "Processing...", "Please wait...", false, false);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            JSONObject result = null;
+                            loading.dismiss();
+                            try {
+
+                                //Parsing the fetched Json String to JSON Object
+                                result = new JSONObject(s);
+                                String status = result.getString("status");
+                                String msg = result.getString("msg");
+                                if (status.equals("success")) {
+
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(OtpForgetActivity.this, msg, Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            //Dismissing the progress dialog
+                            loading.dismiss();
+                            Log.d("error", volleyError.getMessage() + "--" + url.toString());
+                            Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    //Converting Bitmap to String
+                    //String image = getStringImage(fingerData.FingerImage());
+                    //Creating parameters
+                    Map<String, String> params = new Hashtable<String, String>();
+
+                    params.put("type", "resetpassword");
+                    params.put("vcode", etotp.getText().toString());
+                    params.put("npass",etnpassword.getText().toString());
+                    params.put("cpass", etcpassword.getText().toString());
+                    params.put("process_type", "android");
+                    params.put("email",email);
+                    //returning parameters
+                    return params;
+                }
+            };
+            //Creating a Request Queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            //Adding request to the queue
+            requestQueue.add(stringRequest);
+        }
+    }
+    private boolean validatenpassword() {
+        final String data = etnpassword.getText().toString();
+        final String cpass = etcpassword.getText().toString();
+        final String newotp=  etotp.getText().toString();
+        // Check if mobile is entered
+        if (data.length() == 0) {
+            if (!tilnpassword.isErrorEnabled()) {
+                tilnpassword.setErrorEnabled(true);
+            }
+            tilnpassword.setError("New Password Required");
+            return false;
+        }else if (cpass.length() == 0) {
+            if (!tilcpassword.isErrorEnabled()) {
+                tilnpassword.setErrorEnabled(false);
+                tilcpassword.setErrorEnabled(true);
+            }
+            tilnpassword.setError("Conform Password Required");
+            return false;
+        } else if (!vcode.equals(newotp)) {
+            if (!tilcpassword.isErrorEnabled()) {
+                tilcpassword.setErrorEnabled(false);
+                tilotp.setErrorEnabled(true);
+            }
+            tilotp.setError("Security Code Is Incorrect");
+            return false;
+        } else {
+            tilnpassword.setErrorEnabled(false);
+            tilcpassword.setErrorEnabled(false);
+            tilotp.setErrorEnabled(false);
+            return true;
+        }
+    }
+
 }
