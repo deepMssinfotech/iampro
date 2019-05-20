689b1beaf3fcd8d42f0661f1e21ce4dc896a49c1
mraganksoni <mragankshekhar@mssinfotech.com>
2019-05-20 17:14:11 +0530

    sdsafss

--- app/src/main/java/com/mssinfotech/iampro/co/common/PhotoFullPopupWindow.java
+++ app/src/main/java/com/mssinfotech/iampro/co/common/PhotoFullPopupWindow.java
@@ -35,12 +35,9 @@ public class PhotoFullPopupWindow extends PopupWindow {
     ViewGroup parent;
     private static PhotoFullPopupWindow instance = null;
 
-
-
     public PhotoFullPopupWindow(Context ctx, int layout, View v, String imageUrl, Bitmap bitmap) {
         super(((LayoutInflater) ctx.getSystemService(LAYOUT_INFLATER_SERVICE)).inflate( R.layout.popup_photo_full, null), ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT);
-
         if (Build.VERSION.SDK_INT >= 21) {
             setElevation(5.0f);
         }
